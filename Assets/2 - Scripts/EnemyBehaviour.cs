f011896f4d3682ad0f68c259ac2e66eb642c1aa6
Pixel Man <2004pmora@gmail.com>
2025-09-30 12:48:59 -0300

    Avance 30/09
    State Machine añadida, tiene 4 estados: Idle, Walking, Chasing, Searching. Profundizado sistema de NavMesh

--- Assets/2 - Scripts/EnemyBehaviour.cs
+++ Assets/2 - Scripts/EnemyBehaviour.cs
@@ -3,15 +3,114 @@ using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    #region Variables
     NavMeshAgent enemyAgent;
-    public Transform patrolPoint;
+    [SerializeField] Transform[] patrolPoints;
+    [SerializeField] private ENEMY_STATE currentState;
+    public enum ENEMY_STATE
+    {
+        Idle,
+        Walking,
+        Chasing,
+        Searching
+    }
 
-    void Awake()
+    [Header("Idle State")]
+    public float idleTime; //Time for idling
+    public Vector2 minMaxIdleTime; //Range for idleTime
+    float elapsedIdleTime;
+
+    [Header("Chasing State")]
+    //[SerializeField] bool isPlayerOnRange;
+    public Transform player;
+
+    [Header("Searhing State")]
+    public float searchTime; //Time for searching
+    public Vector2 minMaxSearchTime; //Range for searchingTime
+    float elapsedSearchTime;
+    #endregion
+
+    private void Awake()
     {
         enemyAgent = GetComponent<NavMeshAgent>();
     }
-    void Update()
+    private void Start()
     {
-        enemyAgent.SetDestination(patrolPoint.position);
+        idleTime = Random.Range(minMaxIdleTime.x, minMaxIdleTime.y);
+    }
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case ENEMY_STATE.Idle: //Estado de espera
+                idleTime = Random.Range(minMaxIdleTime.x, minMaxIdleTime.y);
+                elapsedIdleTime += Time.deltaTime;
+                if (elapsedIdleTime >= idleTime)
+                {
+                    elapsedIdleTime = 0;
+                    print("We is walking");
+                    ChangeState(ENEMY_STATE.Walking);
+                }
+                break;
+
+            case ENEMY_STATE.Walking: //Estado de caminata
+                if (enemyAgent.remainingDistance <= enemyAgent.stoppingDistance)
+                {
+                    print("Arrived to point");
+                    ChangeState(ENEMY_STATE.Idle);
+                }
+                break;
+
+            case ENEMY_STATE.Chasing: //Estado de persecución
+                print("We is chasing");
+                enemyAgent.SetDestination(player.position);
+                break;
+
+            case ENEMY_STATE.Searching: //Estado de búsqueda tras perder al jugador
+                searchTime = Random.Range(minMaxSearchTime.x, minMaxSearchTime.y);
+                elapsedSearchTime += Time.deltaTime;
+                if (elapsedSearchTime >= idleTime)
+                {
+                    elapsedSearchTime = 0;
+                    print("We is searching");
+                    ChangeState(ENEMY_STATE.Walking);
+                }
+                else if (enemyAgent.remainingDistance <= enemyAgent.stoppingDistance)
+                {
+                    print("Arrived to point");
+                    ChangeState(ENEMY_STATE.Idle);
+                }
+                break;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("Found player");
+            ChangeState(ENEMY_STATE.Chasing);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("Lost Player");
+            ChangeState(ENEMY_STATE.Searching);
+        }
+    }
+    void ChangeState(ENEMY_STATE newState)
+    {
+        currentState = newState;
+        switch (newState)
+        {
+            case ENEMY_STATE.Walking:
+                enemyAgent.SetDestination(patrolPoints[Random.Range(0, patrolPoints.Length)].position);
+                break;
+
+            case ENEMY_STATE.Chasing:
+                enemyAgent.SetDestination(player.position);
+                break;
+        }
     }
 }
