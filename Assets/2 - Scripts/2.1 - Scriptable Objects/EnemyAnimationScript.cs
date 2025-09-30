ff1bd29f2fcdc88827c3525b6878577df903f235
Meaz308 <martin.e.arenas.z@gmail.com>
2025-09-30 12:44:46 -0300

    Logica de animación Idle y loop en animaciones

--- Assets/2 - Scripts/2.1 - Scriptable Objects/EnemyAnimationScript.cs
+++ Assets/2 - Scripts/2.1 - Scriptable Objects/EnemyAnimationScript.cs
@@ -4,4 +4,12 @@ public class EnemyAnimationScript : MonoBehaviour
 {
     public Animator anim;
 
+    private void Start()
+    {
+        anim.SetBool("EnemyIdle", true);
+    }
+    private void Update()
+    {
+        
+    }
 }
