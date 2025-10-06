b29644fad42683d99206ff0421e1202c9994e587
Meaz308 <martin.e.arenas.z@gmail.com>
2025-10-06 16:20:16 -0300

    Sliders agregados

---
+++ Assets/2 - Scripts/VolumeScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeScript : MonoBehaviour
+{
+    public AudioMixer SFXMixer;
+    public AudioMixer BGMMixer;
+    public Slider SFXSlider;
+    public Slider BGMSlider;
+
+    private void Update()
+    {
+        
+    }
+}
