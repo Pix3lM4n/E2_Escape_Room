92ca5821923f25c6bf915aa624f646c07408bad2
Meaz308 <martin.e.arenas.z@gmail.com>
2025-10-06 17:23:17 -0300

    Pantalla de pausa funcional

--- Assets/2 - Scripts/PauseScript.cs
+++ Assets/2 - Scripts/PauseScript.cs
@@ -3,25 +3,54 @@ using UnityEngine.UI;
 
 public class PauseScript : MonoBehaviour
 {
+    private GameObject pauseScreen;
+    private GameObject menuUI;
     private GameObject settingsUI;
     
 
     private void Awake()
     {
+        pauseScreen = GameObject.Find("Pause Screen");
+        menuUI = GameObject.Find("MenuUI");
         settingsUI = GameObject.Find("SettingsUI");
     }
 
     private void Start()
     {
+        pauseScreen.SetActive(false);
+        menuUI.SetActive(false);
         settingsUI.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            OpenMenu();
+        }
+    }
+
+    public void OpenMenu()
+    {
+        Time.timeScale = 0;
+        pauseScreen.SetActive(true);
+        menuUI.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
+        menuUI.SetActive(false);
+    }
     public void OpenSettings()
     {
+        menuUI.SetActive(false);
         settingsUI.SetActive(true);
     }
     public void HideSettings()
     {
+        menuUI.SetActive(true);
         settingsUI.SetActive(false);
     }
 }
